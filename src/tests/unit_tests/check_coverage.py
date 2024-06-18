ad1b16226e89ff2222384e75b0204ad4e7486208
hhvergara <hhvservice@gmail.com>
2024-06-18 02:27:21 -0300

    Adding coverage threshold

--- src/tests/unit_tests/check_coverage.py
+++ src/tests/unit_tests/check_coverage.py
@@ -2,6 +2,11 @@ import json
 import sys
 
 def check_coverage(threshold):
+    bright_red = '\033[91m'
+    bright_green = '\033[92m'
+    bright_yellow = '\033[93m'
+    reset = '\033[0m'
+
     try:
         with open('coverage.json') as f:
             data = json.load(f)
@@ -9,22 +14,22 @@ def check_coverage(threshold):
         coverage = data['totals']['percent_covered']
 
         if coverage is None:
-            print("No coverage information found.")
+            print(f"{bright_red}No coverage information found.{reset}")
             sys.exit(1)
 
         print(f"Coverage: {coverage}%")
 
         if coverage < threshold:
-            print(f"Coverage is below threshold. Expected at least {threshold}%, but got {coverage}%.")
+            print(f"{bright_red}Coverage is below threshold. Expected at least {threshold}%, but got {coverage}%.{reset}")
             sys.exit(1)
         else:
-            print(f"Coverage meets the threshold: {coverage}% >= {threshold}%")
+            print(f"{bright_green}Coverage meets the threshold: {coverage}% >= {threshold}%{reset}")
 
     except json.JSONDecodeError:
-        print("Failed to parse coverage JSON report.")
+        print(f"{bright_red}Failed to parse coverage JSON report.{reset}")
         sys.exit(1)
     except FileNotFoundError:
-        print("Coverage JSON report not found.")
+        print(f"{bright_red}Coverage JSON report not found.{reset}")
         sys.exit(1)
 
 if __name__ == "__main__":
